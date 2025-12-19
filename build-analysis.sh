d56439ae674cd3cb6b73fa9b970ae33828c44fd3
Venki <42660556+iveevi@users.noreply.github.com>
2025-12-18 23:14:17 -0500

    Remove samples and related submodules

--- build-analysis.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-rm -rf build
-export CC=clang CXX=clang++
-cmake -B build -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON .
-cmake --build build -j 1 -t prototype
-clang-build-analyzer --all build trace.txt
-clang-build-analyzer --analyze trace.txt > analysis.txt
-cat analysis.txt
